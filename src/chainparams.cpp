bcc98d65c978c7d8ea0952f02a6c2136f590e819
zetacoin <zetacoin>
2014-04-15 12:27:39 +0200

    Seed nodes

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -79,6 +79,10 @@ public:
         vSeeds.push_back(CDNSSeedData("seed6.zeta-coin.org", "seed6.zeta-coin.org"));
         vSeeds.push_back(CDNSSeedData("seed7.zeta-coin.org", "seed7.zeta-coin.org"));
         vSeeds.push_back(CDNSSeedData("seed8.zeta-coin.org", "seed8.zeta-coin.org"));
+        vSeeds.push_back(CDNSSeedData("albs1.zetacoinseed.com", "albs1.zetacoinseed.com"));
+        vSeeds.push_back(CDNSSeedData("albs2.zetacoinseed.com", "albs2.zetacoinseed.com"));
+        vSeeds.push_back(CDNSSeedData("albs3.zetacoinseed.com", "albs3.zetacoinseed.com"));
+        vSeeds.push_back(CDNSSeedData("albs4.zetacoinseed.com", "albs4.zetacoinseed.com"));
 
         base58Prefixes[PUBKEY_ADDRESS] = 80;
         base58Prefixes[SCRIPT_ADDRESS] = 9;
